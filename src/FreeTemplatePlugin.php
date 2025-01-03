1dc598a8c84829107baf3202ff5b7d034d114bb8
Daniel Dahme <dahme@qualitus.de>
2025-01-03 11:37:11 +0100

    servicelocator flags, strict types

--- src/FreeTemplatePlugin.php
+++ src/FreeTemplatePlugin.php
@@ -1,16 +1,17 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace FreeTemplate;
 
 use Api\IPlugin;
 use Api\ICheck;
+use Base3\ServiceLocator;
 
 class FreeTemplatePlugin implements IPlugin, ICheck {
 
 	private $servicelocator;
 
 	public function __construct() {
-		$this->servicelocator = \Base3\ServiceLocator::getInstance();
+		$this->servicelocator = ServiceLocator::getInstance();
 	}
 
 	// Implementation of IBase
@@ -24,8 +25,8 @@ class FreeTemplatePlugin implements IPlugin, ICheck {
 	public function init() {
 
 		$this->servicelocator
-			->set($this->getName(), $this, true)
-			;
+
+			->set($this->getName(), $this, ServiceLocator::SHARED);
 	}
 
 	// Implementation of ICheck
